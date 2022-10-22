8087184d27f56dfb46c1ddc1cc7a01acbf4255bb
Jesse French <frejes32@gmail.com>
2022-10-21 23:57:32 -0500

    updated static folder route, modified index page text, and added gif

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
@@ -24,7 +25,7 @@ connectDB()
 app.set('view engine', 'ejs')
 
 // Static folder
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Body parsing
 app.use(express.urlencoded({ extended: true }))
