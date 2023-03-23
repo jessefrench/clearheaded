9d9061c34510e9b42402f0ca834e015874a2c8cb
Jesse French <frejes32@gmail.com>
2023-03-23 13:17:12 -0500

    fixed issue with passport middleware causing app to crash when logging out

--- config/passport.js
+++ config/passport.js
@@ -3,31 +3,36 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
-      if (err) { return done(err) }
+  passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email: email.toLowerCase() })
       if (!user) {
         return done(null, false, { msg: `Email ${email} not found.` })
       }
       if (!user.password) {
         return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
       }
-      user.comparePassword(password, (err, isMatch) => {
-        if (err) { return done(err) }
-        if (isMatch) {
-          return done(null, user)
-        }
+      const isMatch = await user.comparePassword(password)
+      if (isMatch) {
+        return done(null, user)
+      } else {
         return done(null, false, { msg: 'Invalid email or password.' })
-      })
-    })
+      }
+    } catch (err) {
+      return done(err)
+    }
   }))
-  
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id)
+      done(null, user)
+    } catch (err) {
+      done(err, null)
+    }
   })
 }
--- models/User.js
+++ models/User.js
@@ -22,9 +22,13 @@ const UserSchema = new mongoose.Schema({
 })
 
 // Helper method for validating user's password
-UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch)
+UserSchema.methods.comparePassword = function(candidatePassword) {
+  const currentPassword = this.password
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, currentPassword, function(err, isMatch) {
+        if (err) return reject(err)
+        resolve(isMatch)
+    })
   })
 }
 
