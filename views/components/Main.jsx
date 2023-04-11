eaeda7cdb30402b18f0d7c419ff931e3ec9c525b
Jesse French <frejes32@gmail.com>
2023-04-11 18:44:39 -0500

    removed type=module from main.js script tag

--- views/components/Main.jsx
+++ views/components/Main.jsx
@@ -18,7 +18,7 @@ export default function Main(props){
         </head>
         <body>
           {props.children}
-          <script type='module' src={'../js/main.js'}></script>
+          <script src={'../js/main.js'}></script>
         </body>
       </html>
     </>
